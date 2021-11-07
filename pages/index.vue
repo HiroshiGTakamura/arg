40a61144fbb8f227867a4266d36b4a23bbba8a72
Luis Henrique Moraes <luishmoraes@yahoo.com.br>
2021-11-06 21:52:29 -0300

    fixed login page 2

--- pages/index.vue
+++ pages/index.vue
@@ -3,32 +3,30 @@ const router = useRouter();
 const log = ref("");
 const pass = ref("");
 const err = ref(false);
-const verify = {
-	ver: function () {
-		if (log.value.toLowerCase() == "ginasantos@gmail.com" && pass.value == "va260295") {
-			// router.push("/game");
-			window.location.pathname = "/game";
-			err = false;
-		} else {
-			err.value = true;
-		}
-	},
+const verify = function () {
+	if (log.value.toLowerCase() == "ginasantos@gmail.com" && pass.value == "va260295") {
+		// router.push("/game");
+		window.location.pathname = "/game";
+		err = false;
+	} else {
+		err.value = true;
+	}
 };
 </script>
 <template>
 	<Navbar />
 	<div class="flex login h-52 flex-col gap-4 m-auto translate-y-1/2">
-		<div>
+		<div class="flex-shrink">
 			<h1>Login</h1>
 			<input class="border-2 border-gray-700 rounded-sm" v-model="log" placeholder="me@email.com" />
 		</div>
-		<div>
+		<div class="flex-shrink">
 			<h1>Senha</h1>
 			<input class="border-2 border-gray-700 rounded-sm" v-model="pass" placeholder="*********" />
 		</div>
 
-		<div class="flex justify-between mt-2">
-			<button @click="verify.ver()" class="text-white bg-blue-500 button hover:bg-blue-600">Entrar</button>
+		<div class="flex flex-shrink justify-between mt-2">
+			<button @click="verify()" class="text-white bg-blue-500 button hover:bg-blue-600">Entrar</button>
 		</div>
 		<div v-if="err.valueOf()">
 			<h1 class="mt-3 bg-red-300 border-2 border-red-700 rounded-md">ERRO: Login ou senha estão INCORRETOS.</h1>
