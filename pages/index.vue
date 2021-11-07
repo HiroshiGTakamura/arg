f05dad4466f37b30ef7ca63fbadd21416fe5cf8b
Luis Henrique Moraes <luishmoraes@yahoo.com.br>
2021-11-06 21:47:08 -0300

    fixed login page

--- pages/index.vue
+++ pages/index.vue
@@ -17,20 +17,21 @@ const verify = {
 </script>
 <template>
 	<Navbar />
-	<div class="p-8 text-justify">
-		<div class="flex items-center justify-center">
-			<div class="login h-40">
-				<h1>Login</h1>
-				<input class="border-2 border-gray-700 rounded-sm" v-model="log" placeholder="me@email.com" />
-				<h1>Senha</h1>
-				<input class="border-2 border-gray-700 rounded-sm" v-model="pass" placeholder="*********" />
-				<div class="flex justify-between mt-2">
-					<button @click="verify.ver()" class="text-white bg-blue-500 button hover:bg-blue-600">Entrar</button>
-				</div>
-				<div v-if="err.valueOf()">
-					<h1 class="mt-3 bg-red-300 border-2 border-red-700 rounded-md">ERRO: Login ou senha estão INCORRETOS.</h1>
-				</div>
-			</div>
+	<div class="flex login h-52 flex-col gap-4 m-auto translate-y-1/2">
+		<div>
+			<h1>Login</h1>
+			<input class="border-2 border-gray-700 rounded-sm" v-model="log" placeholder="me@email.com" />
+		</div>
+		<div>
+			<h1>Senha</h1>
+			<input class="border-2 border-gray-700 rounded-sm" v-model="pass" placeholder="*********" />
+		</div>
+
+		<div class="flex justify-between mt-2">
+			<button @click="verify.ver()" class="text-white bg-blue-500 button hover:bg-blue-600">Entrar</button>
+		</div>
+		<div v-if="err.valueOf()">
+			<h1 class="mt-3 bg-red-300 border-2 border-red-700 rounded-md">ERRO: Login ou senha estão INCORRETOS.</h1>
 		</div>
 	</div>
 </template>
