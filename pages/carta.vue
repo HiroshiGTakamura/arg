00d3a0ba5cb2ddbf662e55504d86a9bd9198f4ac
HiroshiGTakamura <pedrohiroshigodenytakamura@gmail.com>
2021-11-09 19:44:07 -0300

    page final adicionada, format. das caixas de texto

---
+++ pages/carta.vue
@@ -0,0 +1,5 @@
+<template>
+	<div class="p-40">
+		<h1 class="p-2 text-4xl font-bold border-2 border-black rounded-md">carta aqui</h1>
+	</div>
+</template>
--- pages/game.vue
+++ pages/game.vue
@@ -14,46 +14,45 @@ const verify = function () {
 	for (let i = 0; i < nameFields.length; i++) {
 		if (nameFields[i].value.toLowerCase() != names[i]) {
 			console.log("incorreto");
-
 			return;
 		}
 	}
-
-	console.log("correto");
+	router.push("/carta");
+	/*console.log("correto");*/
 };
 </script>
 <template>
 	<title>...</title>
-	<div class="flex flex-col h-full w-full align-center bg-black">
-		<div class="flex justify-center gap-8 items-center flex-col md:flex-row p-8">
-			<img class="bg-white w-32" src="~/assets/tile000.png" />
-			<input class="h-7 w-60" type="text" v-model="name1" />
+	<div class="flex flex-col w-full h-full bg-black align-center">
+		<div class="flex flex-col items-center justify-center gap-8 p-8 md:flex-row">
+			<img class="w-32 bg-white" src="~/assets/tile000.png" />
+			<input class="font-bold h-7 w-60" type="text" v-model="name1" />
 		</div>
-		<div class="flex justify-center gap-8 items-center flex-col md:flex-row p-8">
-			<img class="bg-white w-32" src="~/assets/tile001.png" />
-			<input class="h-7 w-60" type="text" v-model="name2" />
+		<div class="flex flex-col items-center justify-center gap-8 p-8 md:flex-row">
+			<img class="w-32 bg-white" src="~/assets/tile001.png" />
+			<input class="font-bold h-7 w-60" type="text" v-model="name2" />
 		</div>
-		<div class="flex justify-center gap-8 items-center flex-col md:flex-row p-8">
-			<img class="bg-white w-32" src="~/assets/tile002.png" />
-			<input class="h-7 w-60" type="text" v-model="name3" />
+		<div class="flex flex-col items-center justify-center gap-8 p-8 md:flex-row">
+			<img class="w-32 bg-white" src="~/assets/tile002.png" />
+			<input class="font-bold h-7 w-60" type="text" v-model="name3" />
 		</div>
-		<div class="flex justify-center gap-8 items-center flex-col md:flex-row p-8">
-			<img class="bg-white w-32" src="~/assets/tile003.png" />
-			<input class="h-7 w-60" type="text" v-model="name4" />
+		<div class="flex flex-col items-center justify-center gap-8 p-8 md:flex-row">
+			<img class="w-32 bg-white" src="~/assets/tile003.png" />
+			<input class="font-bold h-7 w-60" type="text" v-model="name4" />
 		</div>
-		<div class="flex justify-center gap-8 items-center flex-col md:flex-row p-8">
-			<img class="bg-white w-32" src="~/assets/tile004.png" />
-			<input class="h-7 w-60" type="text" v-model="name5" />
+		<div class="flex flex-col items-center justify-center gap-8 p-8 md:flex-row">
+			<img class="w-32 bg-white" src="~/assets/tile004.png" />
+			<input class="font-bold h-7 w-60" type="text" v-model="name5" />
 		</div>
-		<div class="flex justify-center gap-8 items-center flex-col md:flex-row p-8">
-			<img class="bg-white w-32" src="~/assets/tile005.png" />
-			<input class="h-7 w-60" type="text" v-model="name6" />
+		<div class="flex flex-col items-center justify-center gap-8 p-8 md:flex-row">
+			<img class="w-32 bg-white" src="~/assets/tile005.png" />
+			<input class="font-bold h-7 w-60" type="text" v-model="name6" />
 		</div>
-		<div class="flex justify-center gap-8 items-center flex-col md:flex-row p-8">
-			<img class="bg-white w-32" src="~/assets/tile006.png" />
-			<input class="h-7 w-60" type="text" v-model="name7" />
+		<div class="flex flex-col items-center justify-center gap-8 p-8 md:flex-row">
+			<img class="w-32 bg-white" src="~/assets/tile006.png" />
+			<input class="font-bold h-7 w-60" type="text" v-model="name7" />
 		</div>
 
-		<button @click="verify()" class="text-black bg-white button hover:bg-gray-300 w-36 self-center m-8 p-3">Enviar</button>
+		<button @click="verify()" class="self-center p-3 m-8 text-black bg-white button hover:bg-gray-300 w-36">Enviar</button>
 	</div>
 </template>
